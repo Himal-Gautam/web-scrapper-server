40ce2b4e06130de4f4fee98fb07c3a0e796aff6b
Himal Gautam <himalpgautam@gmail.com>
2022-04-03 13:49:10 +0530

    .

--- src/scripts/scrapper.js
+++ src/scripts/scrapper.js
@@ -43,48 +43,64 @@ const fetchAmazonProducts = async () => {
   }
 };
 
-fetchAmazonProducts().then((shelves) => {
-  console.log(shelves)
+fetchAmazonProducts().then((products) => {
+  console.log(products)
     try {
       Product.deleteMany({})
-      Product.insertMany(shelves);
+      Product.insertMany(products);
     } catch (e) {
       console.log('not inserted')
     }
   });
 
-// const fetchFlipkartProducts = async () => {
-//     try {
-//         const response = await axios.get('https://www.amazon.in/s?k=electronic&crid=2RFQ12NV3CQF4&sprefix=electronic%2Caps%2C579&ref=nb_sb_noss');
-
-//         const html = response.data;
-
-//         const $ = cheerio.load(html);
-
-//         const shelves = [];
-
-//         $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.s-widget-spacing-small.sg-col-4-of-20').each((_idx, el) => {
-//             const shelf = $(el)
-//             // const title = shelf.find('span.a-size-medium.a-color-base.a-text-normal').text()
-//             // const image = shelf.find('img.s-image').attr('src')
-//             // const stars = shelf.find('div.a-section.a-spacing-none.a-spacing-top-micro > div > span').attr('aria-label')
-
-//     // const price = shelf.find('span.a-price > span.a-offscreen').text()
-
-//             let element = {
-//                 // title,
-//                 // image,
-//                 // stars,
-//                 // price
-//             }
-
-//             shelves.push(element)
-//         });
-
-//         return shelves;
-//     } catch (error) {
-//         throw error;
-//     }
-//  };
-
-// fetchFlipkartProducts().then((shelves) => console.log(shelves));
+  // const fetchFlipkartProducts = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://www.amazon.in/s?k=electronic&crid=2RFQ12NV3CQF4&sprefix=electronic%2Caps%2C579&ref=nb_sb_noss"
+  //     );
+  
+  //     const html = response.data;
+  
+  //     const $ = cheerio.load(html);
+  
+  //     const products = [];
+  
+  //     $(
+  //       "div._4ddWPX"
+  //     ).each((_idx, el) => {
+  //       const item = $(el);
+  //       const title = item.find('span.a-size-base-plus.a-color-base.a-text-normal').text()
+  //       const image = item.find('img._396cs4._3exPp9').attr('src')
+  //       const rating = parseInt(item.find('div._3LWZlK').attr('aria-label'))
+  //       const price = item.find('div._3Ay6Sb').text()
+  //       const finalPrice = item.find('div._30jeq3').text()
+  
+  //       console.log(isNaN(rating))
+  
+  //       let element = {
+  //         title,
+  //         image,
+  //         rating,
+  //         price,
+  //         finalPrice
+  //       };
+  
+  //       products.push(element);
+  //     });
+  
+  //     return products.filter((item) => !isNaN(item.rating));
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
+  
+  // fetchFlipkartProducts().then((products) => {
+  //   console.log("FLIPKART")
+  //   console.log(products)
+  //     try {
+  //       Product.deleteMany({})
+  //       Product.insertMany(products);
+  //     } catch (e) {
+  //       console.log('not inserted')
+  //     }
+  //   });
