9f0f892d9af5ff9c35e54c06f74d92d7b4f55767
Himal Gautam <himalpgautam@gmail.com>
2022-04-03 12:04:32 +0530

    .

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import "dotenv/config";
 
 import productRouter from "./routers/product.js";
+import { append } from "cheerio/lib/api/manipulation";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,9 +16,14 @@ app.use(cors());
 app.use(express.json());
 app.use(productRouter);
 
+app.get("/", async (req, res) => {
+  response.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(
     chalk.magenta.bold.underline("Server is up on port ") +
       chalk.blue.bold.underline(port)
   );
 });
+
